8bd991af3b37b17299f4a776779fe676e92e0251
Raigo Aljas <raigo.aljas@tktk.ee>
2026-05-31 19:07:09 +0300

    add week 16 files

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (request, response) => {
+    const jsonFilePath = path.join(__dirname, 'data', 'cvdata.json');
+
+    fs.readFile(jsonFilePath, (error, dataFromFile) => {
+        if(error) {
+            console.error('Failed to read file', error)
+            return response.status(500).send('Failed to read from file');
+        }
+        const jsonData = JSON.parse(dataFromFile);
+        response.render('index', {cv: jsonData});
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('server is running on port: ' + PORT);
+});
