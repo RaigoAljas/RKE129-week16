754376399835ee30bd6cb8b01057803de08cce5c
Raigo Aljas <raigo.aljas@tktk.ee>
2026-05-31 19:14:31 +0300

    uuendus week16

--- public/app.js
+++ public/app.js
@@ -1,53 +1,8 @@
-
-const fNameSpan = document.querySelector('#fName');
-const contactParagraph = document.querySelector('#contactDetails');
-const experienceParagraph = document.querySelector('#experience');
-const educationParagraph = document.querySelector('#education');
-const qualificationParagraph = document.querySelector('#qualification');
-const languagesParagraph = document.querySelector('#languages');
-const interestsList = document.querySelector('#interests');
-
-
 const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
 const targetImage = document.querySelector('.cv-image img');
 let i = 1;
 
-
-window.onload = () => {
-
-   
-    fetch('cvdata.json')
-    .then(response => response.json())
-    .then(data => {
-
-        console.log(data);
-
-       
-        fNameSpan.textContent = data.fName;
-        
-        
-        contactParagraph.innerHTML = data.fName + " " + data.lName + "<br>" + 
-                                     data.email + "<br>" + 
-                                     data.address + "<br>" + 
-                                     data.phone;
-
-        experienceParagraph.textContent = data.experience;
-        educationParagraph.textContent = data.education;
-        qualificationParagraph.textContent = data.qualification;
-        languagesParagraph.innerHTML = data.languages; 
-
-       
-        for(let j = 0; j < data.interests.length; j++) {
-            let li = document.createElement('li');
-            li.innerHTML = data.interests[j];
-            interestsList.appendChild(li);
-        }
-    });
-};
-
-
 targetImage.addEventListener('click', () => {
-
     targetImage.style.opacity = '0';
 
     setTimeout(() => {
@@ -60,5 +15,4 @@ targetImage.addEventListener('click', () => {
 
         targetImage.style.opacity = '1';
     }, 200);
-
 });
